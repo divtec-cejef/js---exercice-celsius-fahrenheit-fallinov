f45b5bc4cc837e8ad867990ec4b4e72d12985bfb
Fallet Steve <steve.fallet@divtec.ch>
2021-09-21 16:23:47 +0200

    ajout résultat

--- js/main.js
+++ js/main.js
@@ -14,8 +14,27 @@
   let fahrenheit = celcius * 9 / 5 + 32;
   alert(`${fahrenheit} °F`);*/
 
+const formulaire = document.querySelector('form');
+const txtCelsius = formulaire.querySelector('#celsius');
+const strongFahrenheit = document.querySelector('.res_fah');
+const ulHistorique = document.querySelector('.historique');
 
+console.log(formulaire, txtCelsius, strongFahrenheit, ulHistorique);
 
+// Envoi du formulaire
+formulaire.addEventListener('submit', function envoiForm(event) {
+  // Annule le comportement par défaut. ici n'envoie pas le form
+  event.preventDefault();
+
+  // Récupère la saisie du visiteur et la transforme en float
+  let celcius = parseFloat(txtCelsius.value);
+  // Calcule et affiche le résultat dans <strong>
+  strongFahrenheit.innerHTML += (celcius * 9 / 5 + 32) + '°F';
+  // Vide le champ texte
+  txtCelsius.value = '';
+  // Redonne le focus au champ
+  txtCelsius.focus();
+});
 
 }()); // Main IIFE
 
